b981acc3049bd78259f4ebe9e21bd0a71b31b878
Stephen Fluin <stephen.fluin@gmail.com>
2025-01-28 11:32:02 -0600

    feat: ui simplificatoin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,33 @@
-'use client';
-import Footer from 'src/components/Footer';
-import TransactionWrapper from 'src/components/TransactionWrapper';
-import WalletWrapper from 'src/components/WalletWrapper';
-import { ONCHAINKIT_LINK } from 'src/links';
-import OnchainkitSvg from 'src/svg/OnchainkitSvg';
-import { useAccount } from 'wagmi';
-import LoginButton from '../components/LoginButton';
-import SignupButton from '../components/SignupButton';
+"use client";
+import Footer from "src/components/Footer";
+import TransactionWrapper from "src/components/TransactionWrapper";
+import WalletWrapper from "src/components/WalletWrapper";
+import { ONCHAINKIT_LINK } from "src/links";
+import OnchainkitSvg from "src/svg/OnchainkitSvg";
+import { useAccount } from "wagmi";
+import LoginButton from "../components/LoginButton";
+import SignupButton from "../components/SignupButton";
+import { SwapDefault } from "@coinbase/onchainkit/swap";
+import type { Token } from "@coinbase/onchainkit/token";
+
+const eth: Token = {
+  name: "ETH",
+  address: "",
+  symbol: "ETH",
+  decimals: 18,
+  image: "https://wallet-api-production.s3.amazonaws.com/uploads/tokens/eth_288.png",
+  chainId: 8453,
+};
+
+const usdc: Token = {
+  name: "USDC",
+  address: "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913",
+  symbol: "USDC",
+  decimals: 6,
+  image:
+    "https://d3r81g40ycuhqg.cloudfront.net/wallet/wais/44/2b/442b80bd16af0c0d9b22e03a16753823fe826e5bfd457292b55fa0ba8c1ba213-ZWUzYjJmZGUtMDYxNy00NDcyLTg0NjQtMWI4OGEwYjBiODE2",
+  chainId: 8453,
+};
 
 export default function Page() {
   const { address } = useAccount();
@@ -15,14 +36,6 @@ export default function Page() {
     <div className="flex h-full w-96 max-w-full flex-col px-1 md:w-[1008px]">
       <section className="mt-6 mb-6 flex w-full flex-col md:flex-row">
         <div className="flex w-full flex-row items-center justify-between gap-2 md:gap-0">
-          <a
-            href={ONCHAINKIT_LINK}
-            title="onchainkit"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <OnchainkitSvg />
-          </a>
           <div className="flex items-center gap-3">
             <SignupButton />
             {!address && <LoginButton />}
@@ -30,23 +43,12 @@ export default function Page() {
         </div>
       </section>
       <section className="templateSection flex w-full flex-col items-center justify-center gap-4 rounded-xl bg-gray-100 px-2 py-4 md:grow">
-        <div className="flex h-[450px] w-[450px] max-w-full items-center justify-center rounded-xl bg-[#030712]">
-          <div className="rounded-xl bg-[#F3F4F6] px-4 py-[11px]">
-            <p className="font-normal text-indigo-600 text-xl not-italic tracking-[-1.2px]">
-              npm install @coinbase/onchainkit
-            </p>
-          </div>
-        </div>
         {address ? (
-          <TransactionWrapper address={address} />
+          <SwapDefault from={[eth]} to={[usdc]} />
         ) : (
-          <WalletWrapper
-            className="w-[450px] max-w-full"
-            text="Sign in to transact"
-          />
+          <WalletWrapper className="w-[450px] max-w-full" text="Sign in to transact" />
         )}
       </section>
-      <Footer />
     </div>
   );
 }
